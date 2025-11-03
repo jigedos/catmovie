100d232801769124ef3ef30bcae2e22d7e416198
jigedos <87001834+jigedos@users.noreply.github.com>
2025-11-03 12:24:04 +0800

    Create vod.ts

---
+++ vod.ts
@@ -0,0 +1,131 @@
+import { writeFileSync } from 'fs'
+import { join } from 'path'
+import { getAvailableCategoryWithCfg } from "./vod_utils"
+import { req } from "utils"
+
+const vods = <Iconfig[]>[
+  {
+    id: "niuniuziyuan",
+    name: "牛牛视频",
+    api: "https://api.niuniuzy.me/api.php/provide/vod",
+    nsfw: false,
+    logo: "https://api.niuniuzy.me/template/niuniuzy/static/images/logo.png",
+    type: 0,
+    extra: {
+      gfw: false,
+    },
+  },
+  {
+    id: "feifanziyuan",
+    name: "非凡资源",
+    api: "http://cj.ffzyapi.com/api.php/provide/vod/at/xml",
+    logo: "http://cj.ffzyapi.com/template/ffzy/static/picture/logo.png",
+    nsfw: false,
+    type: 0,
+    extra: {
+      gfw: false,
+    },
+  },
+
+
+ {
+    id: "xinlang",
+    name: "新浪资源",
+    api: "https://api.xinlangapi.com/xinlangapi.php/provide/vod/at/xml/",
+    nsfw: false,
+    logo: "https://www.xinlangzy.net/template/xinlangzy/image/logo.png",
+    type: 0,
+    extra: {
+      gfw: false,
+    },
+  },
+ {
+    id: "jszy",
+    name: "极速资源",
+    api: "https://jszyapi.com/api.php/provide/vod",
+    nsfw: false,
+    logo: "https://www.jisuzy.com/template/default/images/site_logo.png",
+    type: 0,
+    extra: {
+      gfw: false,
+    },
+  },
+ {
+    id: "rycj",
+    name: "如意资源",
+    api: "https://cj.rycjapi.com/api.php/provide/vod/?ac=list",
+    nsfw: false,
+    logo: "https://www.ryzy.tv/template/lzizycom/static/picture/logo.png",
+    type: 0,
+    extra: {
+      gfw: false,
+    },
+  },
+
+ {
+    id: "wujin",
+    name: "无尽资源",
+    api: "https://api.wujinapi.me/api.php/provide/vod/from/wjm3u8/at/xml/",
+    nsfw: false,
+    logo: "https://www.wujinzy.net/template/ziyuan2/images/logo.jpg",
+    type: 0,
+    extra: {
+      gfw: false,
+    },
+  },
+
+ {
+    id: "iqiyizy",
+    name: "爱奇艺",
+    api: "https://iqiyizyapi.com/api.php/provide/vod",
+    nsfw: false,
+    logo: "https://www.iqiyi.com/static/2025123171318/media/logo.0f63ac5d8ac0e4d5c368.svg",
+    type: 0,
+    extra: {
+      gfw: false,
+    },
+  },
+
+ {
+    id: "xzyb",
+    name: " 幸资源",
+    api: "https://xzybb1.com/api.php/provide/vod/at/xml",
+    nsfw: false,
+    logo: "https://xzybb1.com/template/m1938pc3/image/logo.gif",
+    type: 0,
+    extra: {
+      gfw: false,
+    },
+  },];
+const nsfwVods: Iconfig[] = [
+  {
+    id: "Xxibaoziyuan",
+    name: "X细胞资源",
+    api: "https://www.xxibaozyw.com/api.php/provide/vod",
+    nsfw: true,
+    type: 0,
+    extra: {
+      gfw: false,
+    },
+  },
+  {
+    id: "91shipin",
+    name: "91视频",
+    api: "https://91av.cyou/api.php/provide/vod/",
+    nsfw: true,
+    type: 0,
+    extra: {
+      gfw: true,
+    },
+  },
+] 
+// from args context
+const args = process.argv.slice(2)
+const vodFile = args[0]
+const nsfwVodFile = args[1]
+const file1 = join(process.cwd(), vodFile)
+const file2 = join(process.cwd(), nsfwVodFile);
+
+(async () => {
+  writeFileSync(file1, JSON.stringify(vods, null, 2))
+  writeFileSync(file2, JSON.stringify(nsfwVods, null, 2))
